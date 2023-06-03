683a24a98e737f1db243006d34672d800e31ed74
chuongcyper <chuongcyper@gmail.com>
2023-06-03 08:49:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
+import streamlit as st
+
+# Task Management
+def task_management():
+    st.header("Task Management")
+    # Code for task creation, display, and reminders goes here
+
+# Meeting Scheduler
+def meeting_scheduler():
+    st.header("Meeting Scheduler")
+    # Code for meeting scheduling and notifications goes here
+
+# Resource Management
+def resource_management():
+    st.header("Resource Management")
+    # Code for resource request form, approval, and tracking goes here
+
+# Performance Evaluation
+def performance_evaluation():
+    st.header("Performance Evaluation")
+    # Code for performance evaluation forms, goal setting, and reporting goes here
+
+# Main App
+def main():
+    st.title("Workplace Management App")
+    menu_options = ["Task Management", "Meeting Scheduler", "Resource Management", "Performance Evaluation"]
+    selected_option = st.sidebar.selectbox("Select an option", menu_options)
+
+    if selected_option == "Task Management":
+        task_management()
+    elif selected_option == "Meeting Scheduler":
+        meeting_scheduler()
+    elif selected_option == "Resource Management":
+        resource_management()
+    elif selected_option == "Performance Evaluation":
+        performance_evaluation()
+
+if __name__ == '__main__':
+    main()
 
